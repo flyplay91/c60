0bb1e2a43cc4a9c1c2d8c51e02e04626005c98da
flyplay91 <flyplay91@gmail.com>
2021-03-12 05:59:11 -0500

    added testimonial breadcrumb

--- wp-content/themes/twentytwenty-child/template-parts/testimonial.php
+++ wp-content/themes/twentytwenty-child/template-parts/testimonial.php
@@ -13,6 +13,21 @@ get_header();
 
 <main class="testimonial-page">
 
+    <?php if( have_rows('breadcrumb_group')) : ?>
+        <?php while ( have_rows('breadcrumb_group')): the_row(); ?>
+            <div class="testimonial-breadcrumb inner-section-1366">
+                <?php if( have_rows('breadcrumb_repeater') ) :
+                    while( have_rows('breadcrumb_repeater') ) : the_row();
+                    $title = get_sub_field('title');
+                    $link = get_sub_field('link');
+                ?>
+                    <a href="<?php echo $link ?>"><?php echo $title ?> <span>/</span> </a>
+                    <?php endwhile;
+                endif; ?>
+            </div>
+        <?php endwhile;
+    endif; ?>
+
     <?php if( have_rows('testimonial_hero_group')) :
         $testimonial_hero_group = get_field('testimonial_hero_group');
         $hero_heading = $testimonial_hero_group['hero_heading'];
