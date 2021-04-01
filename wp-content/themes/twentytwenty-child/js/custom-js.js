9fdba090f4da721c131da7245159ab0bd853458e
flyplay91 <flyplay91@gmail.com>
2021-04-01 11:53:00 -0400

    added low stock badge on shop page

--- wp-content/themes/twentytwenty-child/js/custom-js.js
+++ wp-content/themes/twentytwenty-child/js/custom-js.js
@@ -393,19 +393,20 @@ $(document).ready(function() {
 		}
 	});
 
-	if (($('.ast-stock-detail').length != 0) && ($('.product-image-summary__inner .onsale').length != 0)) {
+	if ($('.ast-stock-detail').length > 0 && $('.product-image-summary__inner .onsale').length > 0) {
 		$('.product-image-summary__inner .onsale').hide();
 		$('.product-image-summary__inner').append('<span class="low-qty-badget"></span>');
-	} else if ((($('.ast-stock-detail').length != 0) && ($('.product-image-summary__inner .onsale').length == 0))) {
+	} else if ($('.ast-stock-detail').length > 0 && $('.product-image-summary__inner .onsale').length == 0) {
 		$('.product-image-summary__inner').append('<span class="low-qty-badget"></span>');
 	}
 
 	$('.shop-products-block li').each(function() {
-		if (($(this).find('.single-qty').length != 0) && ($(this).find('.onsale').length != 0)) {
-			$(this).find('.onsale').hide();
-			$(this).find('.woocommerce-LoopProduct-link').append('<span class="low-qty-badget"></span>');
-		} else if (($(this).find('.single-qty').length != 0) && ($(this).find('.onsale').length == 0)) {
-			$(this).find('.woocommerce-LoopProduct-link').append('<span class="low-qty-badget"></span>');
+		let $this = $(this);
+		if ($this.find('.single-qty').length > 0 && $this.find('.onsale').length > 0) {
+			$this.find('.onsale').hide();
+			$this.find('.woocommerce-LoopProduct-link').append('<span class="low-qty-badget"></span>');
+		} else if ($this.find('.single-qty').length > 0 && $this.find('.onsale').length == 0) {
+			$this.find('.woocommerce-LoopProduct-link').append('<span class="low-qty-badget"></span>');
 		}
 	});
 
