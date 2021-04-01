587ce47c1e902cfc98eb483d2b369b8a552e2d27
flyplay91 <flyplay91@gmail.com>
2021-04-01 11:55:44 -0400

    added low stock badge on shop page

--- wp-content/themes/twentytwenty-child/js/custom-js.js
+++ wp-content/themes/twentytwenty-child/js/custom-js.js
@@ -363,6 +363,23 @@ $(document).ready(function() {
 				$('.one-time-option-details .woocommerce-Price-amount').append('<bdi class="subscription-regular-price"> — <span>' + regularPrice +'</span></bdi>');
 			}
 		}	
+
+		if ($('.ast-stock-detail').length > 0 && $('.product-image-summary__inner .onsale').length > 0) {
+			$('.product-image-summary__inner .onsale').hide();
+			$('.product-image-summary__inner').append('<span class="low-qty-badget"></span>');
+		} else if ($('.ast-stock-detail').length > 0 && $('.product-image-summary__inner .onsale').length == 0) {
+			$('.product-image-summary__inner').append('<span class="low-qty-badget"></span>');
+		}
+	
+		$('.shop-products-block li').each(function() {
+			let $this = $(this);
+			if ($this.find('.single-qty').length > 0 && $this.find('.onsale').length > 0) {
+				$this.find('.onsale').hide();
+				$this.find('.woocommerce-LoopProduct-link').append('<span class="low-qty-badget"></span>');
+			} else if ($this.find('.single-qty').length > 0 && $this.find('.onsale').length == 0) {
+				$this.find('.woocommerce-LoopProduct-link').append('<span class="low-qty-badget"></span>');
+			}
+		});
 	}, 1000);
 
 	
@@ -393,22 +410,7 @@ $(document).ready(function() {
 		}
 	});
 
-	if ($('.ast-stock-detail').length > 0 && $('.product-image-summary__inner .onsale').length > 0) {
-		$('.product-image-summary__inner .onsale').hide();
-		$('.product-image-summary__inner').append('<span class="low-qty-badget"></span>');
-	} else if ($('.ast-stock-detail').length > 0 && $('.product-image-summary__inner .onsale').length == 0) {
-		$('.product-image-summary__inner').append('<span class="low-qty-badget"></span>');
-	}
-
-	$('.shop-products-block li').each(function() {
-		let $this = $(this);
-		if ($this.find('.single-qty').length > 0 && $this.find('.onsale').length > 0) {
-			$this.find('.onsale').hide();
-			$this.find('.woocommerce-LoopProduct-link').append('<span class="low-qty-badget"></span>');
-		} else if ($this.find('.single-qty').length > 0 && $this.find('.onsale').length == 0) {
-			$this.find('.woocommerce-LoopProduct-link').append('<span class="low-qty-badget"></span>');
-		}
-	});
+	
 
 	$('body').on('click', '.wcsatt-options-product li input', function() {
 		if ($(this).closest('li').hasClass('one-time-option')) {
